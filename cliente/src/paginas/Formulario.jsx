96687d49c3f4b401a055ce7081437575cbcbadd9
Agustin Migliorero <agustinmigliorero@hotmail.com>
2024-09-04 21:09:45 -0300

    form react

--- cliente/src/paginas/Formulario.jsx
+++ cliente/src/paginas/Formulario.jsx
@@ -5,9 +5,47 @@ function Formulario() {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
 
+  function manejarFormulario(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/api/usuarios", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre: nombre,
+        password: password,
+        email: email,
+      }),
+    });
+  }
+
   return (
     <>
-      <button>Hola!</button>
+      <form onSubmit={manejarFormulario}>
+        <input
+          type="text"
+          placeholder="Nombre"
+          name="nombre"
+          onChange={(e) => setNombre(e.target.value)}
+          value={nombre}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="Clave"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     </>
   );
 }
